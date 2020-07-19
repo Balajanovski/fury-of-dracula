c5ba9cd97f4afde86ca8c16ae63d7f55026d7c56
z5195012 <finnbutton@gmail.com>
2020-07-19 20:03:56 +1000

    HunterView

--- HunterView.c
+++ HunterView.c
@@ -24,105 +24,6 @@
 // TODO: ADD YOUR OWN STRUCTS HERE
 
 struct hunterView {
-	// TODO: ADD FIELDS HERE
-};
-
-////////////////////////////////////////////////////////////////////////
-// Constructor/Destructor
-
-HunterView HvNew(char *pastPlays, Message messages[])
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	HunterView new = malloc(sizeof(*new));
-	if (new == NULL) {
-		fprintf(stderr, "Couldn't allocate HunterView!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	return new;
-}
-
-void HvFree(HunterView hv)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	free(hv);
-}
-
-////////////////////////////////////////////////////////////////////////
-// Game State Information
-
-Round HvGetRound(HunterView hv)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
-}
-
-Player HvGetPlayer(HunterView hv)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return PLAYER_LORD_GODALMING;
-}
-
-int HvGetScore(HunterView hv)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
-}
-
-int HvGetHealth(HunterView hv, Player player)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return 0;
-}
-
-PlaceId HvGetPlayerLocation(HunterView hv, Player player)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return NOWHERE;
-}
-
-PlaceId HvGetVampireLocation(HunterView hv)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	return NOWHERE;
-}
-
-////////////////////////////////////////////////////////////////////////
-// Utility Functions
-
-PlaceId HvGetLastKnownDraculaLocation(HunterView hv, Round *round)
-{
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	*round = 0;
-	return NOWHERE;
-}
-
-////////////////////////////////////////////////////////////////////////
-// COMP2521 20T2 ... the Fury of Dracula
-// HunterView.c: the HunterView ADT implementation
-//
-// 2014-07-01	v1.0	Team Dracula <cs2521@cse.unsw.edu.au>
-// 2017-12-01	v1.1	Team Dracula <cs2521@cse.unsw.edu.au>
-// 2018-12-31	v2.0	Team Dracula <cs2521@cse.unsw.edu.au>
-// 2020-07-10   v3.0    Team Dracula <cs2521@cse.unsw.edu.au>
-//
-////////////////////////////////////////////////////////////////////////
-
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "Game.h"
-#include "GameView.h"
-#include "HunterView.h"
-#include "Map.h"
-#include "Places.h"
-// add your own #includes here
-
-// TODO: ADD YOUR OWN STRUCTS HERE
-
-struct hunterView {
     GameView gv;
 
 };
@@ -246,4 +147,7 @@ PlaceId *HvWhereCanTheyGoByType(HunterView hv, Player player,
 // Your own interface functions
 
 // TODO
+
+
+
 
