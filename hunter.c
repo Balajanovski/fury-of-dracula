90c9660ba309bc5f3c240e9969d75f9a0abd0b91
AndyH <andrew.hu36@gmail.com>
2020-08-08 15:57:39 +1000

    Minor corrections

--- hunter.c
+++ hunter.c
@@ -184,7 +184,7 @@ void decideHunterMove(HunterView hv)
 
 		int isOccup = 0;
 		for (int j = 0; j < 3; j++) {
-			if (occupied[j] == i) isOccup = 1;
+			if (occupied[j] == canGo[i]) isOccup = 1;
 		}
 
 		if (isOccup == 1) continue;
@@ -203,7 +203,6 @@ void decideHunterMove(HunterView hv)
 		int pathLen = -1, shortestLen = 999;
 		for (int i = 0; i < NUM_REAL_PLACES; i++) {
 			if (dist_prob[i] == highestProb && i != curr_loc) {
-
 				PlaceId *shortest = HvGetShortestPathTo(hv,curr_player,(PlaceId)i,&pathLen);
 				if (pathLen < shortestLen && pathLen >= 2) {
 					shortestLen = pathLen;
